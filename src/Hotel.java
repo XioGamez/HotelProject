adedb55b4d89677f6b0d77bca56d2f24a648106a
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-07-26 18:46:30 -0700

    SCRUM-28 (COMPLETED): Created and connected to a database, hotel.db

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,3 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Hotel {
     
+    Connection con;
+
+    public Hotel() {
+        try {
+            con = DriverManager.getConnection("jdbc:sqlite:hotel.db");
+            System.out.println("con.isValid(0) = " + con.isValid(0));
+        }
+        catch(SQLException e) {
+            System.out.println("You fucked up somewhere");
+        }
+    }
 }
