127891f0fd1158abec5d8d2724d2d060878d4995
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-07-27 22:20:53 -0700

    Added a statement to Hotel.java so that it can use REFERENCES

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Hotel {
@@ -9,6 +10,8 @@ public class Hotel {
     public Hotel() {
         try {
             con = DriverManager.getConnection("jdbc:sqlite:hotel.db");
+            PreparedStatement pragma = con.prepareStatement("PRAGMA foreign_keys = ON;");
+            pragma.execute();
             System.out.println("con.isValid(0) = " + con.isValid(0));
         }
         catch(SQLException e) {
