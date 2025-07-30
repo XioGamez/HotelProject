a469170cbbe20d91c90388ea16f898dd5a852718
Leslie C <leslie.cruz.442@my.csun.edu>
2025-07-30 16:02:50 -0700

    Created Scene 0,with login/signup fxml and controller, Edited Scene 1 fxml and controller

--- src/Scene0Controller.java
+++ src/Scene0Controller.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -6,12 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Scene3Controller {
-
+public class Scene0Controller {
+    
     private Stage stage;
     private Scene scene;
 
-    public void goToScene1(ActionEvent event) {
+    public void switchToScene1(ActionEvent event) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -23,16 +24,4 @@ public class Scene3Controller {
         }
     }
 
-    public void goToScene2(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
-
