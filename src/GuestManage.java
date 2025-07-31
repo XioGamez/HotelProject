604ae2636ecbbb299f01903de534042b00d15560
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-07-30 20:30:33 -0700

    Created GuestManage.java, and added addLogin() and authLogin() to it.

---
+++ src/GuestManage.java
@@ -0,0 +1,74 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GuestManage {
+    
+    Connection con;
+
+    public GuestManage() {
+        connect();
+    }
+
+    public void connect() {
+        try {
+        con = DriverManager.getConnection("jdbc:sqlite:hotel.db");
+        PreparedStatement pragma = con.prepareStatement("PRAGMA foreign_keys = ON;");
+        pragma.execute();
+        System.out.println("Connected to database");
+        }
+        
+        catch(SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
+
+    public void addLogin(Login log, Guest guest) {
+        try {
+            if (con == null || con.isClosed()) {
+                connect();
+            }
+            PreparedStatement prst = con.prepareStatement("INSERT INTO login(username,user_id,password-hash) VALUES(?,?,?)");
+            prst.setString(1, log.getUsername());
+            prst.setString(2, guest.getEmail());
+            prst.setInt(3,log.getHashcode());
+            prst.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Boolean authLogin(Login log) {
+        try {
+            if (con == null || con.isClosed()) {
+                connect();
+            }
+            PreparedStatement prst = con.prepareStatement("SELECT FROM login WHERE username = ?");
+            prst.setString(1,log.getUsername());
+            ResultSet rs = prst.executeQuery();
+
+            if(!rs.next()) {
+                return false;
+            }
+            else {
+                if(rs.getInt(3) != log.getHashcode()) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+    
+}
