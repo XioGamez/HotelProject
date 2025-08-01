91a4b4c09fa86d9eac3174aeb4433dfb9aa08931
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-08-01 13:50:13 -0700

    Added a getInfo() to GuestManager.java, and also multiple try-with-resources to any method that requires a connection to the database.

--- src/GuestManager.java
+++ src/GuestManager.java
@@ -9,82 +9,54 @@ public class GuestManager {
     Connection con;
 
     public GuestManager() {
-        connect();
-    }
-
-    public void connect() {
-        try {
-        con = DriverManager.getConnection("jdbc:sqlite:hotel.db");
-        PreparedStatement pragma = con.prepareStatement("PRAGMA foreign_keys = ON;");
-        pragma.execute();
-        System.out.println("Connected to database");
-        }
-        
-        catch(SQLException e) {
-            System.out.println("Connection failed: " + e.getMessage());
-        }
     }
 
     public void signUp(Login log, Guest guest) throws SQLException {
-        try {
-            if (con == null || con.isClosed()) {
-                connect();
-            }
+        try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
 
-            PreparedStatement prst = con.prepareStatement("SELECT user_id FROM login WHERE user_id = ?");
-            prst.setString(1,guest.getEmail());
-            ResultSet rs = prst.executeQuery();
-            if(!rs.next()) {
-
-                prst = con.prepareStatement("INSERT INTO login(username,user_id,hash) VALUES(?,?,?)");
-                prst.setString(1, log.getUsername());
-                prst.setString(2, guest.getEmail());
-                prst.setInt(3,log.getHashcode());
-                prst.executeUpdate();
-
-                prst = con.prepareStatement("Insert INTO Guest(id,name,partySize) VALUES(?,?,?)");
+            try(PreparedStatement prst = con.prepareStatement("SELECT user_id FROM login WHERE user_id = ?")) {
                 prst.setString(1,guest.getEmail());
-                prst.setString(2,guest.getName());
-                prst.setInt(3,guest.getPartySize());
-            }
-            else {
-                System.out.println("You are already a member");
+                
+                    try(ResultSet rs = prst.executeQuery()) {
+                        if(!rs.next()) {
+
+                        try(PreparedStatement prst2 = con.prepareStatement("INSERT INTO login(username,user_id,hash) VALUES(?,?,?)")) {
+                            prst2.setString(1, log.getUsername());
+                            prst2.setString(2, guest.getEmail());
+                            prst2.setInt(3,log.getHashcode());
+                            prst2.executeUpdate();
+                        }
+
+                        try(PreparedStatement prst3 = con.prepareStatement("Insert INTO Guest(id,name,partySize) VALUES(?,?,?)")) {
+                            prst3.setString(1,guest.getEmail());
+                            prst3.setString(2,guest.getName());
+                            prst3.setInt(3,guest.getPartySize());
+                            prst3.executeUpdate();
+                        }
+                    }
+                    else {
+                        System.out.println("You are already a member");
+                    }
+                }
             }
         }
         catch(SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        con.close();
     }
 
     public Boolean login(Login log) throws SQLException {
-        try {
-            if (con == null || con.isClosed()) {
-                connect();
-            }
-            PreparedStatement prst = con.prepareStatement("SELECT username, password-hash FROM login WHERE username = ?");
-            prst.setString(1,log.getUsername());
-            ResultSet rs = prst.executeQuery();
+        try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
 
-            if(!rs.next()) {
-                rs.close();
-                prst.close();
-                con.close();
-                return false;
-            }
-            else {
-                if(rs.getInt(3) != log.getHashcode()) {
-                    rs.close();
-                    prst.close();
-                    con.close();
-                    return false;
-                }
-                else {
-                    rs.close();
-                    prst.close();
-                    con.close();
-                    return true;
+            try(PreparedStatement prst = con.prepareStatement("SELECT * FROM login WHERE username = ?")) {
+                prst.setString(1,log.getUsername());
+
+                try(ResultSet rs = prst.executeQuery()) {
+                    if(!rs.next()) {
+                        return false;
+                    }
+                    return rs.getInt("hash") == log.getHashcode();
                 }
             }
         }
@@ -93,6 +65,25 @@ public class GuestManager {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public Guest getInfo(Login log) throws SQLException {
+        try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
+
+            try(PreparedStatement prst = con.prepareStatement("SELECT * FROM Guest WHERE id = ?")) {
+                prst.setString(1,log.getUserId());
+
+                try(ResultSet rs = prst.executeQuery()) {
+                    if(rs.next()) {
+                        return new Guest(rs.getString("id"),rs.getString("name"),rs.getInt("partySize"));
+                    }
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
     
 }
