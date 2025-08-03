cab19f93ac045d260ca46cb4330a7fa6d3eec045
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-08-02 18:38:24 -0700

    update

--- src/Guest.java
+++ src/Guest.java
@@ -1,38 +1,26 @@
 public class Guest {
     private String name;
-    private int partySize;
     private String email;
-    private String roomType;
+    private int partySize;
     private Payment payment;
 
-    public Guest(String s1, String u1) {
-        this.name = s1;
-        this.email = u1;
+    public Guest(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
+    public Guest(String name, String email, int partySize) {
+        this.name = name;
+        this.email = email;
+        this.partySize = partySize;
 
-    public Guest(String s1, String s2, int num) {
-        this.email = s1;
-        this.partySize = num;
-        this.name = s2;
     }
-
     public String getName() {
-        return this.name;
+        return name;
     }
-
-    public int getPartySize() {
-        return this.partySize;
-    }
-
     public String getEmail() {
-        return this.email;
+        return email;
     }
-
-    public void setRoomType(String s) {
-        this.roomType = s;
-    }
-
-    public String getRoomType() {
-        return this.roomType;
+    public int getPartySize() {
+        return partySize;
     }
 }
