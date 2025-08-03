43d4fde65388fcc69172dc327d02bb656387da5a
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-08-03 13:58:56 -0700

    Created RoomSelectionController.java and finished MenuController.java, addReservation() is now connected to the front end and full functional.

--- src/MenuController.java
+++ src/MenuController.java
@@ -1,7 +1,38 @@
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.EventObject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class MenuController {
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
     Guest guest;
 
     public void setGuest(Guest guest) {
         this.guest = guest;
+    }
+
+    public void makeReservation(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("RoomSelection.fxml"));	
+        root = loader.load();
+
+        RoomSelectionController rsc = loader.getController();
+        rsc.setGuest(this.guest);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
