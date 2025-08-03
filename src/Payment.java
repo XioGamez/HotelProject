ac90c8a66559e65f1570915ba7a23361fddb33bb
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-08-02 18:41:41 -0700

    add veriables and get methods

--- src/Payment.java
+++ src/Payment.java
@@ -1,5 +1,25 @@
 public class Payment {
-    private String cardType;
-    private int cardNum;
-    private String bank;
+    private String paymentID;
+    private String method;
+    private double amount;
+    private String date;
+
+    public Payment(String paymentID, String method, double amount, String date) {
+        this.paymentID = paymentID;
+        this.method = method;
+        this.amount = amount;
+        this.date = date;
+    }
+    public String getPaymentID() {
+        return paymentID;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public String getDate() {
+        return date;
+    }
 }
