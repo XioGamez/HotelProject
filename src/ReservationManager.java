93f85f911b096a24590b2f4e0a662dd4afa6f22d
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-08-06 12:34:54 -0700

    Removed the cancelReservation() method, was not working right. Debug and tested the getReservation() and updateReservation() methods, both methods ran successfully.

--- src/ReservationManager.java
+++ src/ReservationManager.java
@@ -7,24 +7,6 @@ import java.text.SimpleDateFormat;
 
 public class ReservationManager {
 
-    public void CancelReservation(Reservation reservation) {
-        String roomType = reservation.getRoom().getRoomType();
-
-        try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
-
-            try(PreparedStatement prst = con.prepareStatement("UPDATE " + roomType + " SET occupied = ?, guest_email = ? " + "WHERE room_num = (SELECT room_num FROM " + roomType + " WHERE room_num BETWEEN 1 AND 10 AND occupied IS NULL AND guest_email IS NULL " + "LIMIT 1)");) {
-                prst.setBoolean(1,false);
-                prst.setString(1, null);
-            }
-            try(PreparedStatement prst2 = con.prepareStatement("DELETE FROM Reservation WHERE email = ?")) {
-                prst2.setString(1, reservation.getGuest().getEmail());
-            }
-        }
-        catch(SQLException e) {
-            System.out.println(e.getMessage());
-        }
-}
-
     public Reservation getReservation(Guest guest, String checkIn, String checkOut)  { 
         
         try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {                    
@@ -38,21 +20,21 @@ public class ReservationManager {
                 try(ResultSet rs = prst.executeQuery()) {
                     if(rs.next()) {
 
-                        if(rs.getString("Room_Type") == "Standard") {
+                        if(rs.getString("Room_Type").equals("Standard")) {
                             room = new Standard();
                             guest.setPartySize(rs.getInt("PartySize"));
                             return new Reservation(guest, room, checkIn,checkOut);
-                    }
-                    else if(rs.getString("Room_Type") == "Deluxe") {
+                        }
+                        if(rs.getString("Room_Type").equals("Deluxe"))  {
                             room = new Deluxe();
                             guest.setPartySize(rs.getInt("PartySize"));
                             return new Reservation(guest, room, checkIn,checkOut);
-                    }
-                    else {
+                        }
+                        else {
                             room = new Suite();
                             guest.setPartySize(rs.getInt("PartySize"));
                             return new Reservation(guest, room, checkIn,checkOut);
-                    }
+                        }
                     }
                     
                 }
@@ -65,22 +47,55 @@ public class ReservationManager {
     }
 
     public void updateReservation(Reservation newReservation, Reservation oldReservation)  {
-        String roomType = newReservation.getRoom().getRoomType();
 
         try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
-            try(PreparedStatement prst = con.prepareStatement("UPDATE " + roomType + " SET occupied = ?, guest_email = ? " + "WHERE room_num = (SELECT room_num FROM " + roomType + " WHERE room_num BETWEEN 1 AND 10 AND occupied IS NULL AND guest_email IS NULL " + "LIMIT 1)");) {
-                prst.setBoolean(1, newReservation.getOccupancy());
-                prst.setString(2, newReservation.getGuest().getEmail());
-                prst.executeUpdate();
+            
+            if(newReservation.getRoom().getRoomType().equals(oldReservation.getRoom().getRoomType())) {
+                String roomType = newReservation.getRoom().getRoomType();
+                newReservation.setOccupancy(true);
+                
+                try(PreparedStatement prst = con.prepareStatement("UPDATE " + roomType + " SET occupied = ?, guest_email = ? " + "WHERE room_num = (SELECT room_num FROM " + roomType + " WHERE room_num BETWEEN 1 AND 10 AND occupied IS NULL AND guest_email IS NULL " + "LIMIT 1)");) {
+                    prst.setBoolean(1, newReservation.getOccupancy());
+                    prst.setString(2, newReservation.getGuest().getEmail());
+                    prst.executeUpdate();
+                }
+                try(PreparedStatement prst2 = con.prepareStatement("Update Reservation SET email = ?, name = ?, partySize = ?, checkIn = ?, checkOut = ?, Room_Type = ? WHERE email = ?")) {
+                    prst2.setString(1, newReservation.getGuest().getEmail());
+                    prst2.setString(2, newReservation.getGuest().getName());
+                    prst2.setInt(3,newReservation.getGuest().getPartySize());
+                    prst2.setString(4, newReservation.getCheckIn());
+                    prst2.setString(5, newReservation.getCheckOut());
+                    prst2.setString(6,newReservation.getRoom().getRoomType());
+                    prst2.setString(7,oldReservation.getGuest().getEmail());
+                    prst2.executeUpdate();
+                }
             }
-            try(PreparedStatement prst2 = con.prepareStatement("Update Reservation SET email = ?, name = ?, partySize = ?, checkIn = ?, checkOut = ? WHERE email = ?")) {
-                prst2.setString(1, newReservation.getGuest().getEmail());
-                prst2.setString(2, newReservation.getGuest().getName());
-                prst2.setInt(3,newReservation.getGuest().getPartySize());
-                prst2.setString(4, newReservation.getCheckIn());
-                prst2.setString(5, newReservation.getCheckOut());
-                prst2.setString(6,oldReservation.getGuest().getEmail());
-                prst2.executeUpdate();
+            else {
+                String newRoomType = newReservation.getRoom().getRoomType();
+                String oldRoomType = oldReservation.getRoom().getRoomType();
+                newReservation.setOccupancy(true);
+
+                try(PreparedStatement prst3 = con.prepareStatement("UPDATE " + oldRoomType + " SET occupied = ?, guest_email = ? WHERE guest_email = ?");) {
+                    prst3.setBoolean(1, false);
+                    prst3.setString(2,null);
+                    prst3.setString(3,oldReservation.getGuest().getEmail());
+                    prst3.executeUpdate();
+                }
+                try(PreparedStatement prst = con.prepareStatement("UPDATE " + newRoomType + " SET occupied = ?, guest_email = ? " + "WHERE room_num = (SELECT room_num FROM " + newRoomType + " WHERE room_num BETWEEN 1 AND 10 AND occupied = 0 AND guest_email IS NULL " + "LIMIT 1)");) {
+                    prst.setBoolean(1, newReservation.getOccupancy());
+                    prst.setString(2, newReservation.getGuest().getEmail());
+                    prst.executeUpdate();
+                }
+                try(PreparedStatement prst2 = con.prepareStatement("Update Reservation SET email = ?, name = ?, partySize = ?, checkIn = ?, checkOut = ?, Room_Type = ? WHERE email = ?")) {
+                    prst2.setString(1, newReservation.getGuest().getEmail());
+                    prst2.setString(2, newReservation.getGuest().getName());
+                    prst2.setInt(3,newReservation.getGuest().getPartySize());
+                    prst2.setString(4, newReservation.getCheckIn());
+                    prst2.setString(5, newReservation.getCheckOut());
+                    prst2.setString(6,newReservation.getRoom().getRoomType());
+                    prst2.setString(7,oldReservation.getGuest().getEmail());
+                    prst2.executeUpdate();
+                }
             }
         }
         catch(SQLException e) {
@@ -93,12 +108,13 @@ public class ReservationManager {
             try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
                 
                 // inserting into db
-                try(PreparedStatement prst = con.prepareStatement("INSERT INTO Reservation(email, name, partySize, checkIn, checkOut) VALUES(?,?,?, ?, ?);");) {
+                try(PreparedStatement prst = con.prepareStatement("INSERT INTO Reservation(email, name, partySize, checkIn, checkOut, Room_Type) VALUES(?, ?, ?, ?, ?, ?);");) {
                     prst.setString(1, reservations.getGuest().getEmail());
                     prst.setString(2, reservations.getGuest().getName());
                     prst.setInt(3, reservations.getGuest().getPartySize());
                     prst.setString(4, reservations.getCheckIn());
                     prst.setString(5, reservations.getCheckOut());
+                    prst.setString(6,reservations.getRoom().getRoomType());
                     prst.executeUpdate();
                 }
 
