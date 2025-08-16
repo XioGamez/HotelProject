e8ebe7abfe5e151bb839e0cfc999ccfe299092e4
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-08-15 23:59:39 -0700

    Fixed the formatting of DisplayControllers.java, to make the important information easier to reach, also added comments.

--- src/frontend/DisplayController.java
+++ src/frontend/DisplayController.java
@@ -92,82 +92,8 @@ public class DisplayController {
             }
         });
     }
-
-    public void display(String str) {
-        promptLabel.setText(str);
-    }
-
-    public LocalDate getStartDate() {
-         return check_In.getValue();
-    }
-    public LocalDate getEndDate() {
-        return check_Out.getValue();
-    }
-    public String getCheckIn() {
-        LocalDate localDate = check_In.getValue();
-        return localDate.format(formatter);
-    }
-    public String getCheckOut() {
-        LocalDate localDate = check_Out.getValue();
-        return localDate.format(formatter);
-    }
-
-    public void setHotelRoom(Hotel room) {
-        this.room = room;
-    }
-
-    public double calculatePrice(double basePrice) {
-        PaymentManager pm = new PaymentManager();
-        return pm.calculatePrice(getStartDate(), getEndDate(), basePrice);
-    }
-
-    public void setOldReservation(Reservation oldreservation) {
-        this.oldReservation = oldreservation;
-    }
-
-    public void setGuest(Guest guest) {
-        this.guest = guest;
-    }
     
-    public void setPayment(Payment p) {
-        this.payment = p;
-    }
-    public void finalizeReservation() {
-        if(standardButton.isSelected()) {
-            this.room = new Standard();
-            this.payment.setRoomType("Standard");
-            this.payment.setAmount(calculatePrice(100));
-            //guest.setPartySize(getPartySize());
-        }
-        else if(deluxeButton.isSelected()) {
-            this.room = new Deluxe();
-            this.payment.setRoomType("Deluxe");
-            this.payment.setAmount(calculatePrice(150));
-            //guest.setPartySize(getPartySize());
-        }
-        else if(suiteButton.isSelected()) {
-            this.room = new Suite();
-            this.payment.setRoomType("Suite");
-            this.payment.setAmount(calculatePrice(200));
-            //guest.setPartySize(getPartySize());
-        }
-    }
-    public void display(){
-        oldRoomLabel.setText(oldReservation.getRoom().getRoomType());
-        oldCheckInLabel.setText(oldReservation.getCheckIn());
-        oldCheckOutLabel.setText(oldReservation.getCheckOut());
-    }
-
-    public void print() {
-        System.out.println(payment.getPaymentID() + " " + payment.getMethod() + " " + payment.getCardNum());
-        System.out.println(payment.getAmount());
-    }
-    public boolean finalCheck() {
-        if((standardButton.isSelected() || deluxeButton.isSelected() || suiteButton.isSelected()) && check_In.getValue() != null && check_Out.getValue() != null) {
-            return true;
-        }
-        return false;
-    }
+    // Update the reservation, tied to 'Update Reservation' button, call 2 helper methods finalCheck(), and finalizeReservation()
     public void update(ActionEvent event) throws IOException {
         if(finalCheck()) {
             finalizeReservation();
@@ -201,35 +127,33 @@ public class DisplayController {
         }
          
     }
-   /*  public void update(ActionEvent event) throws IOException {
-        if(newRoomText.getText().equals("Standard")) {
-            Hotel room = new Standard();
-            this.newReservation = new Reservation(guest, room,newCheckInText.getText(),newCheckOutText.getText());
-            ReservationManager r1 = new ReservationManager();
-            r1.updateReservation(newReservation, oldReservation);
+    // Check to see if all required information has been entered
+    public boolean finalCheck() {
+        if((standardButton.isSelected() || deluxeButton.isSelected() || suiteButton.isSelected()) && check_In.getValue() != null && check_Out.getValue() != null) {
+            return true;
         }
-        else if(newRoomText.getText().equals("Deluxe")) {
-            Hotel room = new Deluxe();
-            this.newReservation = new Reservation(guest, room,newCheckInText.getText(),newCheckOutText.getText());
-            ReservationManager r1 = new ReservationManager();
-            r1.updateReservation(newReservation, oldReservation);
+        return false;
+    }
+    // Finalize all reservation related information
+    public void finalizeReservation() {
+        if(standardButton.isSelected()) {
+            this.room = new Standard();
+            this.payment.setRoomType("Standard");
+            this.payment.setAmount(calculatePrice(100));
         }
+        else if(deluxeButton.isSelected()) {
+            this.room = new Deluxe();
+            this.payment.setRoomType("Deluxe");
+            this.payment.setAmount(calculatePrice(150));
+        }
+        else if(suiteButton.isSelected()) {
+            this.room = new Suite();
+            this.payment.setRoomType("Suite");
+            this.payment.setAmount(calculatePrice(200));
+        }
+    }
 
-        else {
-            Hotel room = new Suite();
-            this.newReservation = new Reservation(guest, room,newCheckInText.getText(),newCheckOutText.getText());
-            ReservationManager r1 = new ReservationManager();
-            r1.updateReservation(newReservation, oldReservation);
-        }
-        
-        root = FXMLLoader.load(getClass().getResource("ReservationConfirmation.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-        
-    }*/
-
+    //Cancel the reservation, tied to 'Cancel Reservation' button
     public void CancelReservation(ActionEvent event) throws IOException{
         ReservationManager r1 = new ReservationManager();
         r1.CancelReservation(oldReservation);
@@ -260,6 +184,53 @@ public class DisplayController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void display(String str) {
+        promptLabel.setText(str);
+    }
+    public LocalDate getStartDate() {
+         return check_In.getValue();
+    }
+    public LocalDate getEndDate() {
+        return check_Out.getValue();
+    }
+    public String getCheckIn() {
+        LocalDate localDate = check_In.getValue();
+        return localDate.format(formatter);
+    }
+    public String getCheckOut() {
+        LocalDate localDate = check_Out.getValue();
+        return localDate.format(formatter);
+    }
+    public void setHotelRoom(Hotel room) {
+        this.room = room;
+    }
+    public double calculatePrice(double basePrice) {
+        PaymentManager pm = new PaymentManager();
+        return pm.calculatePrice(getStartDate(), getEndDate(), basePrice);
+    }
+    public void setOldReservation(Reservation oldreservation) {
+        this.oldReservation = oldreservation;
+    }
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+    public void setPayment(Payment p) {
+        this.payment = p;
+    }
+    public void display(){
+        oldRoomLabel.setText(oldReservation.getRoom().getRoomType());
+        oldCheckInLabel.setText(oldReservation.getCheckIn());
+        oldCheckOutLabel.setText(oldReservation.getCheckOut());
+    }
+    public void print() {
+        System.out.println(payment.getPaymentID() + " " + payment.getMethod() + " " + payment.getCardNum());
+        System.out.println(payment.getAmount());
+    }
+
+    
+
+    
 
 
 }
